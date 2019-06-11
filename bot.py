f6218531bc147da9f7e4f172bca0ea7a1c2ed02e
devkrytoy4el <50165574+devkrytoy4el@users.noreply.github.com>
2019-06-11 17:14:59 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ import asyncio
 import requests
 import random
 
-
 Bot = commands.Bot(command_prefix = '?')
 Bot.remove_command('help')
 token ='NTY5NDYwMDgyMTY1ODA5MTUz.XPvtxg.MTpLkeGzPunQ2NujO3W0g0ZWOAI'
+channel = Bot.get_channel(572075184606150657)
 
 @Bot.event
 async def on_ready():
@@ -25,7 +25,7 @@ async def cat (ctx):
     data = response.json()
     await ctx.send(data['file'])
     print('[?cat] - done. Induced '+ author)
-
+    await channel.send('[?cat] - done. Induced '+ author)
 @Bot.command() 
 async def dog (ctx):
     author = str(ctx.message.author)
@@ -33,7 +33,7 @@ async def dog (ctx):
     data = response.json()
     await ctx.send(data['url'])
     print('[?dog] - done. Induced '+ author)
-
+    await channel.send(print('[?dog] - done. Induced '+ author))
 
 @Bot.command()
 @commands.has_permissions(administrator= True)
@@ -41,11 +41,13 @@ async def clear(ctx, amount: int):
     author = str(ctx.message.author)
     await ctx.channel.purge(limit=amount)
     print("[?clear] - done. Delete " + format(amount) + " message. Induced "+ author)
+    await channel.send("[?clear] - done. Delete " + format(amount) + " message. Induced "+ author)
 @Bot.command()
 async def hi(ctx):
     author = ctx.message.author
     await ctx.send('Привет ,'+ format(author.mention)+ ', ты крутой!')
     print('[?hi] - done. Induced '+ str(author))
+    await chanel.send('[?hi] - done. Induced '+ str(author))
 @Bot.command()
 async def help(ctx):
     author = str(ctx.message.author)
@@ -58,6 +60,7 @@ async def help(ctx):
     emb.add_field(name="?coin", value= "Бросить монетку.", inline=False)
     await ctx.send(embed=emb)
     print("[?help] - done. Induced "+ author)
+    await channel.send("[?help] - done. Induced "+ author)
 
 @Bot.command()
 async def ava(ctx, member : discord.Member = None):
@@ -69,6 +72,7 @@ async def ava(ctx, member : discord.Member = None):
     embed.set_image(url=user.avatar_url)
     await ctx.send(embed=embed)
     print("[?ava] - done. Induced "+ author)
+    await channel.send("[?ava] - done. Induced "+ author)
 @Bot.command(pass_context= True)
 async def rnum(ctx):
     author = str(ctx.message.author) 
@@ -76,6 +80,8 @@ async def rnum(ctx):
     await asyncio.sleep(1)
     await ctx.delete_message(ctx.message)
     print("[?rnum] - done. Induced "+ author)
+    await channel.send("[?rnum] - done. Induced "+ author)
+
 @Bot.command()
 async def coin(ctx):
     author = str(ctx.message.author)
@@ -83,12 +89,12 @@ async def coin(ctx):
     if (num == 1):
        	await ctx.send("Вам выпал - `Орёл`")
        	print("[?coin] - done. Induced "+ author)
+        await channel.send("[?coin] - done. Induced "+ author)
     if(num == 2):	
        	await ctx.send("Вам выпала - `Решка`")
        	print("[?coin] - done. Induced "+ author)
+        await channel.send("[?coin] - done. Induced "+ author)
 
 
 
 Bot.run(token)
-
-
