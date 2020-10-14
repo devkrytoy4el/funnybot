81ee15c135008402ce6ea8f92d260b9e6fd4074f
devkrytoy4el <50165574+devkrytoy4el@users.noreply.github.com>
2020-10-14 13:56:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -117,18 +117,7 @@ async def ahelp(ctx):
     emb.add_field(name="?say", value= "Написать что-либ от лица бота,указывать текст сообщения через пробел после комманды.", inline=False)
     await ctx.send(embed=emb)
     await channel.send("[?ahelp] - done. " + "Induced "+ author)
-
-
-@Bot.command()
-async def lastpost(ctx):
-    channel = Bot.get_channel(600384214822813696)
-    author = str(ctx.message.author)
-    postidlist =requests.get('https://api.vk.com/method/wall.get', params={'domain':'fortnite','count':2,'access_token':tOken,'v':5.52})#получаем нужный пост
-    a=postidlist.json()
-    await ctx.send(a['response']['items'][1]['text'])
-    await channel.send("[?lastpost] - done. Induced "+ author)
-    await ctx.send((a['response']['items'][1]['attachments'][0]['photo']['photo_1280'])
-        
+      
     
 @Bot.command()
 async def coin(ctx):
@@ -140,7 +129,5 @@ async def coin(ctx):
     await channel.send("[?coin] - done. Induced "+ author)
   
 
-
-tOken=os.environ.get('VK_TOKEN')
 token = os.environ.get('BOT_TOKEN')
 Bot.run(str(token))
